38198084dc7b5c4b3153ba337f7614deb5397b41
Anuj Amin <anujamin01@gmail.com>
2023-04-26 17:47:11 -0500

    delete.C completed

--- hw6_src/delete.C
+++ hw6_src/delete.C
@@ -16,6 +16,53 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
+	Status status;
+	// declare hfs object
+	HeapFileScan* hfs = new HeapFileScan(relation,status);
+
+	if (status != OK){
+		return status;
+	}
+	AttrDesc desc;
+	RID rid;
+	// get description of attribute info
+	status = attrCat->getInfo(relation, attrName, desc);
+	if (status != OK){
+		delete hfs;
+		return status;
+	}
+	int offset = desc.attrOffset;
+	int len = desc.attrLen;
+
+	// start scan 
+	if (attrName.empty()){
+		status = hfs->startScan(0, 0, STRING, NULL, EQ);
+	}
+	else if (desc.attrType == STRING){
+		status = hfs->startScan(offset, len, type, attrValue, op);
+	} else if (desc.attrType == INTEGER){
+		int val = atoi(attrValue);
+		status = hfs->startScan(offset, len, type, (char *)&val, op);
+	} else if(desc.attrType == FLOAT){
+		float fval = atof(attrValue);
+		status = hfs->startScan(offset, len, type, (char *)&fval, op);
+	}
+	if (status != OK){
+		delete hfs;
+		return status;
+	}
+
+	// scanNext and try to delete the record
+  	while((status = hfs->scanNext(rid)) == OK) 
+  	{
+    	if ((status = hfs->deleteRecord()) != OK)
+				delete hfs;
+    		return status;
+  	}	
+	// finished scan
+	hfs->endScan();
+	delete hfs;
+
 // part 6
 return OK;
 
