96e057e46506714b0d1f983062052368c95c2178
Anuj Amin <anujamin01@gmail.com>
2023-04-26 18:02:50 -0500

    TODO: ScanSelect in select.C

--- hw6_src/select.C
+++ hw6_src/select.C
@@ -28,7 +28,33 @@ const Status QU_Select(const string & result,
 {
    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
+		Status status;
+		// array to hold descriptions of all projections
+		AttrDesc descs[projCnt];
 
+		// get the needed information for each projection
+		int i = 0;
+		while (i < projCnt){
+			status = attrCat->getInfo(descs[i].relName, descs[i].attrName, descs[i]);
+			if (status != OK){
+				return status;
+			}
+			i++;
+		}
+		int len = 0;
+		AttrDesc* attrPtr = NULL;
+
+		if (attr != NULL){
+			attrPtr = new AttrDesc;
+			status = attrCat->getInfo(attr->relName, attr->attrName, *attrPtr);
+			if (status != OK){
+				return status;
+			}
+			len = attrPtr->attrLen;
+		}
+
+		// scan and find
+		return ScanSelect(result, projCnt, descs, attrPtr,op,attrValue, len);
 }
 
 
