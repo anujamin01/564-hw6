bf98da21952744605d0ef171dd86049290c65664
stevenlai1688 <stevenlai1688@gmail.com>
2023-04-28 18:29:52 -0500

    fixed delete.C, passing test 7

--- hw6_src/delete.C
+++ hw6_src/delete.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Deletes records from a specified relation.
  *
@@ -10,64 +9,67 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
-		       const Operator op,
-		       const Datatype type, 
-		       const char *attrValue)
+const Status QU_Delete(const string &relation,
+					   const string &attrName,
+					   const Operator op,
+					   const Datatype type,
+					   const char *attrValue)
 {
 	Status status;
 	// declare hfs object
-	HeapFileScan* hfs = new HeapFileScan(relation,status);
+	HeapFileScan *hfs = new HeapFileScan(relation, status);
 
-	if (status != OK){
+	if (status != OK)
+	{
 		return status;
 	}
 	AttrDesc desc;
 	RID rid;
 	// get description of attribute info
-	status = attrCat->getInfo(relation, attrName, desc);
-	if (status != OK){
-		delete hfs;
-		return status;
-	}
+	attrCat->getInfo(relation, attrName, desc);
+
 	int offset = desc.attrOffset;
 	int len = desc.attrLen;
-
-	// start scan 
-	if (attrName.empty()){
+	int val;
+	float fval;
+	// start scan
+	// handle attrname null
+	if (attrName.empty())
+	{
 		status = hfs->startScan(0, 0, STRING, NULL, EQ);
 	}
-	else if (desc.attrType == STRING){
+	else if (type == STRING)
+	{
 		status = hfs->startScan(offset, len, type, attrValue, op);
-	} else if (desc.attrType == INTEGER){
-		int val = atoi(attrValue);
+	}
+	else if (type == INTEGER)
+	{
+		val = atoi(attrValue);
 		status = hfs->startScan(offset, len, type, (char *)&val, op);
-	} else if(desc.attrType == FLOAT){
-		float fval = atof(attrValue);
+	}
+	else if (type == FLOAT)
+	{
+		fval = atof(attrValue);
 		status = hfs->startScan(offset, len, type, (char *)&fval, op);
 	}
-	if (status != OK){
+
+	if (status != OK)
+	{
 		delete hfs;
 		return status;
 	}
 
 	// scanNext and try to delete the record
-  	while((status = hfs->scanNext(rid)) == OK) 
-  	{
-    	if ((status = hfs->deleteRecord()) != OK)
-				delete hfs;
-    		return status;
-  	}	
+	while ((status = hfs->scanNext(rid)) == OK)
+	{
+		if ((status = hfs->deleteRecord()) != OK)
+			//	delete hfs;
+			return status;
+	}
 	// finished scan
 	hfs->endScan();
 	delete hfs;
 
-// part 6
-return OK;
-
-
-
+	// part 6
+	return OK;
 }
-
-
