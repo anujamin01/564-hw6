bbd48ce9acdedfb486f188d35610b1878893d679
stevenlai1688 <stevenlai1688@gmail.com>
2023-04-28 18:44:15 -0500

    fixed insert.C and passed test case 5

--- hw6_src/insert.C
+++ hw6_src/insert.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Inserts a record into the specified relation.
  *
@@ -10,61 +9,74 @@
  * 	an error code otherwise
  */
 
-const Status QU_Insert(const string & relation, 
-	const int attrCnt, 
-	const attrInfo attrList[])
+const Status QU_Insert(const string &relation,
+					   const int attrCnt,
+					   const attrInfo attrList[])
 {
 	int attributeCounter;
 	AttrDesc *attributeDescPtr;
 	// use attrCat->getRelInfo to get the attributes in order and their descriptions
 
 	Status status = attrCat->getRelInfo(relation, attributeCounter, attributeDescPtr);
-	if (status != OK){
+	if (status != OK)
+	{
 		return status;
 	}
 	// if the parameter attrCnt is wrong
-	if (attributeCounter != attrCnt){
+	if (attributeCounter != attrCnt)
+	{
 		return UNIXERR;
 	}
 
 	// grab total length of what to insert
 	int relLen = 0;
-	for(int i = 0; i < attrCnt; i++){
+	for (int i = 0; i < attrCnt; i++)
+	{
 		relLen += attributeDescPtr[i].attrLen;
 	}
-	
+
 	InsertFileScan ifs(relation, status);
-	if (status != OK){
+	if (status != OK)
+	{
 		return status;
 	}
 
 	char *insertInfo;
-	if (!(insertInfo = new char[relLen])){
+	if (!(insertInfo = new char[relLen]))
+	{
 		return INSUFMEM;
 	}
 
 	int offset = 0;
 	int val = 0;
 	float fval = 0;
-	bool attrFound = false;
 	// add all the attribute info to our buffer in order
-	int i = 0;
-	while (i < attrCnt){
-		attrFound = false;
-		for (int j = 0; j < attrCnt; j++){
+
+	for (int i = 0; i < attrCnt; i++)
+	{
+		bool attrFound = false;
+		for (int j = 0; j < attrCnt; j++)
+		{
 			// found matching attribute
-			if(strcmp(attributeDescPtr[i].attrName,attributeDescPtr[j].attrName) == 0){
+			if (strcmp(attributeDescPtr[i].attrName, attrList[j].attrName) == 0)
+			{
 				offset = attributeDescPtr[i].attrOffset;
-				if (attrList[j].attrType == STRING){
+				if (attrList[j].attrType == STRING)
+				{
 					memcpy((char *)insertInfo + offset, (char *)attrList[j].attrValue, attributeDescPtr[i].attrLen);
-				} else if(attrList[j].attrType == INTEGER){
+				}
+				else if (attrList[j].attrType == INTEGER)
+				{
 					val = atoi((char *)attrList[j].attrValue);
 					memcpy((char *)insertInfo + offset, &val, attributeDescPtr[i].attrLen);
-
-				} else if(attrList[j].attrType == FLOAT){
+				}
+				else if (attrList[j].attrType == FLOAT)
+				{
 					fval = atof((char *)attrList[j].attrValue);
 					memcpy((char *)insertInfo + offset, &fval, attributeDescPtr[i].attrLen);
-				} else{
+				}
+				else
+				{
 					// some random type
 					return UNIXERR;
 				}
@@ -74,29 +86,28 @@ const Status QU_Insert(const string & relation,
 			}
 		}
 		// error couldn't find a matching attribute
-		if (!attrFound){
-			delete [] insertInfo;
+		if (!attrFound)
+		{
+			delete[] insertInfo;
 			free(attributeDescPtr);
 			return UNIXERR;
 		}
-		i+=1;
 	}
 	// do the insertion
 	Record r;
-	r.data = (void *) insertInfo;
+	r.data = (void *)insertInfo;
 	r.length = relLen;
 
 	RID insertRID;
-	status = ifs.insertRecord(r,insertRID);
-	if (status != OK){
+	status = ifs.insertRecord(r, insertRID);
+	if (status != OK)
+	{
 		return status;
 	}
 
 	// free some stuff up
-	delete [] insertInfo;
+	delete[] insertInfo;
 	free(attributeDescPtr);
-// part 6
-return OK;
 
+	return OK;
 }
-
